d5c17fd90387c7da367f55728bfd0914d1424566
RRTracer <rrtracer@gmail.com>
2024-10-07 18:57:18 +0200

    ajout de listebox avec les check box de chaque option et du bouton de desactivation

--- source/main.cpp
+++ source/main.cpp
@@ -1,25 +1,19 @@
 // Dear ImGui: standalone example application for SDL2 + OpenGL
 // (SDL is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
 
-// Learn about Dear ImGui:
-// - FAQ                  https://dearimgui.com/faq
-// - Getting Started      https://dearimgui.com/getting-started
-// - Documentation        https://dearimgui.com/docs (same as your local docs/ folder).
-// - Introduction, links and more at the top of imgui.cpp
-
 #include "imgui.h"
 #include "imgui_impl_sdl2.h"
 #include "imgui_impl_opengl3.h"
 #include <cmath>
 #include <stdio.h>
 #include <SDL.h>
+
 #if defined(IMGUI_IMPL_OPENGL_ES2)
 #include <SDL_opengles2.h>
 #else
 #include <SDL_opengl.h>
 #endif
 
-// This example can also compile and run with Emscripten! See 'Makefile.emscripten' for details.
 #ifdef __EMSCRIPTEN__
 #include "../libs/emscripten/emscripten_mainloop_stub.h"
 #endif
@@ -36,22 +30,19 @@ int main(int, char**)
 
     // Decide GL+GLSL versions
 #if defined(IMGUI_IMPL_OPENGL_ES2)
-    // GL ES 2.0 + GLSL 100
-    const char* glsl_version = "#version 100";
+    const char* glsl_version = "#version 100"; // GL ES 2.0 + GLSL 100
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 #elif defined(__APPLE__)
-    // GL 3.2 Core + GLSL 150
-    const char* glsl_version = "#version 150";
+    const char* glsl_version = "#version 150"; // GL 3.2 Core + GLSL 150
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG); // Always required on Mac
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 #else
-    // GL 3.0 + GLSL 130
-    const char* glsl_version = "#version 130";
+    const char* glsl_version = "#version 130"; // GL 3.0 + GLSL 130
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
@@ -69,6 +60,7 @@ int main(int, char**)
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
     SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
     SDL_Window* window = SDL_CreateWindow("Dear ImGui SDL2+OpenGL3 example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+
     if (window == nullptr)
     {
         printf("Error: SDL_CreateWindow(): %s\n", SDL_GetError());
@@ -82,27 +74,25 @@ int main(int, char**)
     // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
     // Setup Dear ImGui style
-    ImGui::StyleColorsDark();
-    //ImGui::StyleColorsLight();
+    ImGui::StyleColorsDark(); // Utilisez un style par défaut, vous pouvez changer cela si nécessaire
 
-    // Setup Platform/Renderer backends
+    // Setup Platform/Renderer bindings
     ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
-
+    // Tool activation flags
     bool my_tool_active = true;
+    bool my_tool = true;
 
-    
+    // Variables for resource usage
+    int clicked = 0;
+
     // Main loop
     bool done = false;
 #ifdef __EMSCRIPTEN__
-
-    io.IniFilename = nullptr;
+    ImGui::GetIO().IniFilename = nullptr;
     EMSCRIPTEN_MAINLOOP_BEGIN
 #else
     while (!done)
@@ -111,12 +101,13 @@ int main(int, char**)
         SDL_Event event;
         while (SDL_PollEvent(&event))
         {
-            ImGui_ImplSDL2_ProcessEvent(&event);
+            ImGui_ImplSDL2_ProcessEvent(&event); // Traitez les événements SDL ici
             if (event.type == SDL_QUIT)
                 done = true;
             if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window))
                 done = true;
         }
+
         if (SDL_GetWindowFlags(window) & SDL_WINDOW_MINIMIZED)
         {
             SDL_Delay(10);
@@ -128,60 +119,84 @@ int main(int, char**)
         ImGui_ImplSDL2_NewFrame();
         ImGui::NewFrame();
 
-       
-
+        // Main GUI code
         if (my_tool_active)
         {
-                        // Create a window called "My First Tool", with a menu bar.
-            ImGui::Begin("Linear signal color tools ", &my_tool_active);
+            ImGui::Begin("Linear Signal Color Tools", &my_tool_active);
 
             if (ImGui::BeginMenuBar())
             {
                 if (ImGui::BeginMenu("Analyseur Réseau"))
                 {
                     if (ImGui::MenuItem("Open..", "Ctrl+O")) { /* Do stuff */ }
-                    if (ImGui::MenuItem("Save", "Ctrl+S"))   { /* Do stuff */ }
-                    if (ImGui::MenuItem("Close", "Ctrl+W"))  { my_tool_active = false; }
+                    if (ImGui::MenuItem("Save", "Ctrl+S")) { /* Do stuff */ }
+                    if (ImGui::MenuItem("Close", "Ctrl+W")) { my_tool_active = false; }
                     ImGui::EndMenu();
                 }
                 ImGui::EndMenuBar();
             }
 
-            
             // Generate samples and plot them
             float samples[1000];
             for (int n = 0; n < 1000; n++)
                 samples[n] = sinf(n * 0.4f + ImGui::GetTime() * 1.8f);
             ImGui::PlotLines("Test de 1000", samples, 1000);
 
-            // Generate samples and plot them
             float samples_2[100];
             for (int n = 0; n < 100; n++)
                 samples_2[n] = sinf(n * 0.4f + ImGui::GetTime() * 1.8f);
             ImGui::PlotLines("Test de 100", samples_2, 100);
 
-            // Generate samples and plot them
             float samples_3[10];
             for (int n = 0; n < 10; n++)
                 samples_3[n] = cos(n * 0.3f + ImGui::GetTime() * 1.5f);
             ImGui::PlotLines("Test de 10", samples_3, 10);
-            
-            float floccari[182];
-            for (int i = 0; i<182; i++)
-                floccari[i] = sinf(i * 0.3f + ImGui::GetTime() * 1.8f);
-            ImGui::PlotLines("Test de 182 par sinus", floccari, 182);
-         
+
             ImGui::End();
         }
-        
+
+        if (my_tool)
+        {
+            ImGui::Begin("Linear Signal Color Tools", &my_tool);
+
+            const char* items[] = { "Option A", "Option B", "Option C" };
+            static bool selected_items[IM_ARRAYSIZE(items)] = { false, false, false }; // Checkboxes for each item
+
+            for (int i = 0; i < IM_ARRAYSIZE(items); i++)
+            {
+                ImGui::Checkbox(items[i], &selected_items[i]); // Show the checkbox
+
+                if (selected_items[i])
+                {
+                    ImGui::SameLine();
+                    if (ImGui::Button("Désélectionner"))
+                    {
+                        selected_items[i] = false; // Deselect the checkbox
+                    }
+                }
+            }
+
+            if (ImGui::Button("Close Window"))
+                clicked++;
+
+            if (clicked & 1)
+            {
+                ImGui::SameLine();
+                ImGui::Text("Merci d'avoir utilisé mon logiciel");
+                my_tool = false;
+            }
+
+            ImGui::End();
+        }
+
         // Rendering
         ImGui::Render();
-        glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
-        // glClearColor(glClearColor.x * glClearColor.w, glClearColor.y * glClearColor.w, glClearColor.z * glClearColor.w, glClearColor.w);
+        glViewport(0, 0, (int)ImGui::GetIO().DisplaySize.x, (int)ImGui::GetIO().DisplaySize.y);
         glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         SDL_GL_SwapWindow(window);
     }
+
 #ifdef __EMSCRIPTEN__
     EMSCRIPTEN_MAINLOOP_END;
 #endif
