d55d580cb612b2b07e7fea2e59b80a4254671843
RRTracer <rrtracer@gmail.com>
2024-10-27 22:58:18 +0100

    dernière modif

--- source/main.cpp
+++ source/main.cpp
@@ -15,7 +15,7 @@
 #include <SDL_opengl.h>
 #endif
 
-#ifdef __EMSCRIPTEN__
+#ifdef __EMSCRIPTEN__   
 #include "../libs/emscripten/emscripten_mainloop_stub.h"
 #endif
 
@@ -81,7 +81,7 @@ int main(int, char**) {
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
     SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
-    SDL_Window* window = SDL_CreateWindow("Dear ImGui SDL2+OpenGL3 example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+    SDL_Window* window = SDL_CreateWindow("Mes tools pour la programmation cour M. Bachard .", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
 
     if (window == nullptr) {
         printf("Error: SDL_CreateWindow(): %s\n", SDL_GetError());
